3a31409cd9a75029d377eee0c75e72351b903d4a
Elvis Onobhayedo <elvisonob@yahoo.com>
2022-01-19 17:42:49 +0000

    working

--- src/basket.js
+++ src/basket.js
@@ -4,13 +4,19 @@ class Basket {
         this.basket = [];
         this.basketSize = 5;
     };
-    
+
     addItemToBasket(sku) {
-        for (let i = 0; i < inventory.length; i++) {
-            if (inventory[i].sku === sku && this.basket.length < this.basketSize) {
-                this.basket.push(inventory[i]);
-            }
+        if (this.basket.length < this.basketSize) {
+            const item = inventory.find(inventory => inventory.sku === sku)
+            this.basket.push(item)
         }
+
+
+        // for (let i = 0; i < inventory.length; i++) {
+        //     if (inventory[i].sku === sku && this.basket.length < this.basketSize) {
+        //         this.basket.push(inventory[i]);
+        //     }
+        // }
         return 'You cannot add more than 5 items to your basket!'
     };
 
